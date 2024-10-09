bd34ee8c73df48d54f2855162704af8c997700fd
Alexey Okhrimenko <alexey_nats@mail.ru>
2024-10-09 10:43:48 +0300

    new branch test

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -3,6 +3,8 @@ package ru.otus.java.basic.homeworks;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("new branch");
+
 
     }
 }
