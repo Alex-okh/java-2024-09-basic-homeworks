113377f061300174f7240f0c23171a4c85e1f4a5
Alexey Okhrimenko <alexey_nats@mail.ru>
2024-10-10 22:28:24 +0300

    Homework1

--- src/main/java/ru/otus/java/basic/homeworks/hw1/Solution.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw1/Solution.java
@@ -2,6 +2,43 @@ package ru.otus.java.basic.homeworks.hw1;
 
 public class Solution {
     public static void main(String[] args) {
+        greetings();
+        checkSign(2, 3, -3);
+
+
+    }
+
+    public static void greetings() {
+        System.out.println("Hello\nWorld\nfrom\nJava");
+    }
+
+    public static void checkSign(int a, int b, int c) {
+        if (a + b + c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void selectColor() {
+        int data = 20;
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data <= 20) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 20;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
 
     }
 }
