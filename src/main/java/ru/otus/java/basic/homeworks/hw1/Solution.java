5427ec74b4ee69126cfe246303a40ca61a9d257e
Alexey Okhrimenko <alexey_nats@mail.ru>
2024-10-11 11:13:33 +0300

    Homework1 done

--- src/main/java/ru/otus/java/basic/homeworks/hw1/Solution.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw1/Solution.java
@@ -1,18 +1,50 @@
 package ru.otus.java.basic.homeworks.hw1;
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
         greetings();
         checkSign(2, 3, -3);
+        selectColor();
+        compareNumbers();
+        addOrSubtractAndPrint(10, 4, true);
 
+// (*)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+            Введите номер метода для выполнения:
+            1 - greetings
+            2 - checkSign
+            3 - selectColor
+            4 - compareNumbers
+            5 - addOrSubtractAndPrint
+            """);
+        int choise = sc.nextInt();
+
+        switch (choise) {
+            case 1 -> greetings();
+            case 2 -> checkSign(getRndInt(-100, 100), getRndInt(-100, 100), getRndInt(-100, 100));
+            case 3 -> selectColor();
+            case 4 -> compareNumbers();
+            case 5 -> {
+                boolean inc = false;
+                if (getRndInt(-1, 1) > 0) {
+                    inc = true;
+                }
+                addOrSubtractAndPrint(getRndInt(-100, 100), getRndInt(-100, 100), inc);
+            }
+            default -> System.out.println("Нет такого метода");
+        }
 
     }
-// Задача 1
+
+    // Задача 1
     public static void greetings() {
         System.out.println("Hello\nWorld\nfrom\nJava");
     }
 
-// Задача 2
+    // Задача 2
     public static void checkSign(int a, int b, int c) {
         if (a + b + c >= 0) {
             System.out.println("Сумма положительная");
@@ -33,7 +65,7 @@ public class Solution {
         }
     }
 
-// Задача 4
+    // Задача 4
     public static void compareNumbers() {
         int a = 10;
         int b = 20;
@@ -43,5 +75,19 @@ public class Solution {
             System.out.println("a<b");
         }
 
+    }
+
+    // Задача 5
+    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
+        if (increment) {
+            System.out.println(initValue + delta);
+        } else {
+            System.out.println(initValue - delta);
+        }
+
+    }
+
+    public static int getRndInt(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
     }
 }
