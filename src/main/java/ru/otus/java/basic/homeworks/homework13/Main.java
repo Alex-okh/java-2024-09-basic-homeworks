dc4d92fe719878b8ae96d214b983f174aa4f2ad2
Alexey Okhrimenko <alexey_nats@mail.ru>
2024-12-14 21:36:55 +0300

    Homework14

--- src/main/java/ru/otus/java/basic/homeworks/homework13/Main.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework13/Main.java
@@ -4,7 +4,7 @@ import ru.otus.java.basic.homeworks.homework13.calculator.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        String answer = Calculator.calculate("(2+3)*(3+3)");
+        String answer = Calculator.calculate("(2+3)*(3*(7^4+3*2)-5)^2/1000000");
         System.out.println(answer);
     }
 }
