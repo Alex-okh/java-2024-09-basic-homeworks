e6e04544092c94104d0b50bc2aa3bf2952da8c59
Alexey Okhrimenko <alexey_nats@mail.ru>
2024-12-16 19:22:16 +0300

    Homework13 - refactored.

--- src/main/java/ru/otus/java/basic/homeworks/homework13/Server.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework13/Server.java
@@ -13,76 +13,47 @@ public class Server {
         ServerSocket socket = null;
         try {
             socket = new ServerSocket(8080);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Server started.");
-        Socket clientSocket = null;
-        while (true) {
-            try {
+            System.out.println("Server started.");
+            Socket clientSocket = null;
+            while (true) {
                 clientSocket = socket.accept();
                 clientConnected = true;
                 System.out.println("Client connected. Port : " + clientSocket.getPort());
-            } catch (IOException e) {
-                System.out.println("Exception:  " + e.getMessage());
-            }
-            while (clientConnected) {
-                ClientHelper client = null;
-                try {
-                    client = new ClientHelper(clientSocket);
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-
-                }
-
-                String output = "";
-                String input = "";
-                try {
-                    input = client.recieveonly();
-                } catch (IOException e) {
-                    System.out.println("Exception:  " + e.getMessage());
-                } catch (NullPointerException e) {
-                    System.out.println("Exception:  " + e.getMessage());
-                    clientConnected = false;
-                }
-                System.out.println("Server received: " + input);
-                switch (input) {
-                    case "exit" -> {
-                        try {
-                            client.close();
-                            clientConnected = false;
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
+                while (clientConnected) {
+                    try {
+                        ClientHelper client = new ClientHelper(clientSocket);
+                        String output = "";
+                        String input = "";
+                        input = client.recieveonly();
+                        System.out.println("Server received: " + input);
+                        switch (input) {
+                            case "exit" -> {
+                                client.close();
+                                System.out.println("Connection closed.");
+                            }
+                            case "Command_HELLO" -> output = """
+                                ---------------------------------------------------------------------------------------------------------
+                                Welcome to calculator server v.0.9. Please send a string with an equation to calculate or "exit" to exit.
+                                + - * / ^ operations are supported. Brackets are supported. Please use . or , as decimal separator.
+                                ---------------------------------------------------------------------------------------------------------""";
+                            case "Command_DIE" -> System.exit(0);
+                            default -> output = Calculator.calculate(input);
                         }
-                        System.out.println("Connection closed.");
-                    }
-                    case "Command_HELLO" -> output = """
-                        ---------------------------------------------------------------------------------------------------------
-                        Welcome to calculator server v.0.9. Please send a string with an equation to calculate or "exit" to exit.
-                        + - * / ^ operations are supported. Brackets are supported. Please use . or , as decimal separator.
-                        ---------------------------------------------------------------------------------------------------------""";
-                    case "Command_DIE" -> System.exit(0);
-                    default -> {
-                        try {
-                            output = Calculator.calculate(input);
-                        } catch (Exception e) {
-                            output = "Could not calculate due to internal error. Sorry.";
-                        }
+                        client.sendonly(output);
+                        System.out.println("Server sent: " + output);
+
+                    } catch (RuntimeException | IOException e) {
+                        System.out.println(e.getMessage());
+                        clientConnected = false;
                     }
                 }
-                if (clientConnected) try {
-                    client.sendonly(output);
-                    System.out.println("Server sent: " + output);
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                    clientConnected = false;
-                }
             }
+        } catch (RuntimeException | IOException e) {
+            System.out.println("Could not start server.");
+            System.out.println(e.getMessage());
 
 
         }
-
     }
 }
 
