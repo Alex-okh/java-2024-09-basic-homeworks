fbb650cb4fc31d909c1fbfd023d13c54690f767f
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-01-28 11:34:13 +0300

    repair

--- src/test/java/ru/otus/java/basic/homeworks/homework22/MainAppTest.java
+++ src/test/java/ru/otus/java/basic/homeworks/homework22/MainAppTest.java
@@ -1,15 +1,12 @@
 package ru.otus.java.basic.homeworks.homework22;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,92 +15,54 @@ import static ru.otus.java.basic.homeworks.homework22.MainApp.trimAfterOne;
 
 public class MainAppTest {
 
-  public static Stream<Arguments> goodArraysData() {
+  public static Stream<Arguments> basicTrimData() {
     List<Arguments> data = new ArrayList<>();
-    for (int i = 3; i < 20; i++) {
-      int[] newArray = new int[i];
-      Arrays.fill(newArray, 1);
-      Arrays.fill(newArray, 0, newArray.length / 2, 2);
-      data.add(Arguments.arguments(newArray));
-    }
+    data.add(Arguments.arguments(new int[]{}, new int[]{2, 3, 1}));
+    data.add(Arguments.arguments(new int[]{7, 8}, new int[]{5, 4, 1, 7, 8}));
+    data.add(Arguments.arguments(new int[]{7, 8}, new int[]{1, 5, 4, 1, 7, 8}));
+    data.add(Arguments.arguments(new int[]{5, 4, 7, 8}, new int[]{1, 5, 4, 7, 8}));
+
     return data.stream();
   }
 
-  public static Stream<Arguments> badArraysData() {
+  public static Stream<Arguments> exceptionsTrimData() {
     List<Arguments> data = new ArrayList<>();
-    for (int i = 3; i < 20; i++) {
-      int[] newArray = new int[i];
-      Arrays.fill(newArray, 1);
-      Arrays.fill(newArray, 0, newArray.length / 2, 2);
-      newArray[i - 2] = 4;
-      data.add(Arguments.arguments(newArray));
-    }
+    data.add(Arguments.arguments(new int[]{2, 3, 4}));
+    data.add(Arguments.arguments(new int[]{}));
+    data.add(Arguments.arguments((Object) null));
     return data.stream();
   }
 
-  public static Stream<Arguments> trimData() {
+  public static Stream<Arguments> oneAndTwoData() {
     List<Arguments> data = new ArrayList<>();
-    Random rand = new Random();
-    for (int i = 5; i < 20; i++) {
-      int[] newArray = new int[i];
-      for (int j = 0; j <newArray.length; j++) {
-        newArray[j] = rand.nextInt(2,10);
-      }
-      int onePosition = rand.nextInt(newArray.length-1);
-      newArray[onePosition] = 1;
-      int[] expected = Arrays.copyOfRange(newArray,onePosition+1, newArray.length);
-      data.add(Arguments.arguments(newArray,expected));
-    }
+    data.add(Arguments.arguments(true, new int[]{1, 2}));
+    data.add(Arguments.arguments(true, new int[]{1, 2, 1, 2, 1, 2}));
+    data.add(Arguments.arguments(false, new int[]{1}));
+    data.add(Arguments.arguments(false, new int[]{2}));
+    data.add(Arguments.arguments(false, new int[]{1, 3, 2}));
+    data.add(Arguments.arguments(false, new int[]{}));
+    data.add(Arguments.arguments(false, (Object) null));
     return data.stream();
-  }
-
-  @Test
-  @DisplayName ("Array trim after 1 - Basic tests")
-  public void testTrimAfterOne() {
-    assertArrayEquals(new int[]{}, trimAfterOne(new int[]{2, 3, 1}));
-    assertArrayEquals(new int[]{7, 8}, trimAfterOne(new int[]{5, 4, 1, 7, 8}));
-    assertArrayEquals(new int[]{7, 8}, trimAfterOne(new int[]{1, 5, 4, 1, 7, 8}));
-    assertArrayEquals(new int[]{5, 4, 7, 8}, trimAfterOne(new int[]{1, 5, 4, 7, 8}));
-
-    assertThrows(RuntimeException.class, () -> trimAfterOne(new int[]{2, 3, 4}));
-    assertThrows(RuntimeException.class, () -> trimAfterOne(new int[]{}));
-    assertThrows(RuntimeException.class, () -> trimAfterOne(null));
-
   }
 
   @ParameterizedTest
-  @MethodSource ("trimData")
-  @DisplayName ("Array trim after 1 - Random array tests")
-  public void testTrim(int[] input, int[] expected) {
+  @DisplayName("Parametrized Basic tests")
+  @MethodSource("basicTrimData")
+  void testTrimAfterOne(int[] expected, int[] input) {
     assertArrayEquals(expected, trimAfterOne(input));
   }
 
-  @Test
-  @DisplayName ("1 or 2 only - Basic tests")
-  public void testCheckOneAndTwoOnly() {
-    assertTrue(checkOneAndTwoOnly(new int[]{1, 2}));
-    assertTrue(checkOneAndTwoOnly(new int[]{1, 2, 1, 2, 1, 2}));
-
-    assertFalse(checkOneAndTwoOnly(new int[]{1}));
-    assertFalse(checkOneAndTwoOnly(new int[]{2}));
-    assertFalse(checkOneAndTwoOnly(new int[]{1, 3, 2}));
-    assertFalse(checkOneAndTwoOnly(new int[]{}));
-    assertFalse(checkOneAndTwoOnly(null));
-
+  @ParameterizedTest
+  @DisplayName("Exception should be thrown")
+  @MethodSource("exceptionsTrimData")
+  void testExTrimAfterOne(int[] input) {
+    assertThrows(RuntimeException.class, () -> trimAfterOne(input));
   }
 
   @ParameterizedTest
-  @MethodSource("goodArraysData")
-  @DisplayName ("1 or 2 only - Random valid arrays")
-  public void testCheckOneAndTwoTrue(int[] input) {
-    assertTrue(checkOneAndTwoOnly(input));
-  }
-
-  @ParameterizedTest
-  @MethodSource("badArraysData")
-  @DisplayName ("1 or 2 only - Random invalid arrays")
-  public void testCheckOneAndTwoFalse(int[] input) {
-    assertFalse(checkOneAndTwoOnly(input));
-
+  @DisplayName("1 and 2 only - basic tests")
+  @MethodSource("oneAndTwoData")
+  void testOneAndTwo(boolean expected, int[] input) {
+    assertEquals(expected, checkOneAndTwoOnly(input));
   }
 }
